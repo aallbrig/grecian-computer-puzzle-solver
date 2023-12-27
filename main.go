463af8d9d0d664371862e677c6dbd788439c58ee
Andrew Allbright <aallbrig@gmail.com>
2023-12-27 16:07:00 -0500

    Polish solution

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import "fmt"
 
-// 5 layers, 4 rows, 12 elements in each row
-// top layer to bottom layer, outer radial column to inner radial column
-// -1 means no element
-var grecianComputerPuzzle = [5][4][12]int{
+// Observational notes:
+// 5 layers, 4 "radial" rows, there are 12 elements in each row.
+// The geometry of the puzzle has empty spaces revealing the number on the layer(s) below it.
+// Data collected by reading the top layer all the way to bottom layer, "outer" radial column to "inner" radial column.
+// -1 means no element, I like to think of numbers > 0 as being a "mask" being applied over layers below.
+// While writing down the data I had to decide how I defined "up" for the layer.
+// I decided "up" was the highest number in the outer radial row.
+// Except for the bottom layer, which I instead decided to define "up" as based on the art I found on the real puzzle itself.
+// The cog appears to have a bright spot, with sketch shading to imply shadow. I decided the bright spot was "up".
+var grecianComputerObservationData = [5][4][12]int{
 	{
 		{-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
 		{-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
@@ -42,13 +48,21 @@ type GrecianComputer struct {
 	Puzzle [5][4][12]int
 }
 
+// Solve solves the puzzle as if a person is solving the puzzle for themselves.
 func (gc *GrecianComputer) Solve() {
 	rotationCounts := [5]int{}
 
+	// human-readable algorithm:
+	// rotate the bottom layer 12 times (checking each time) and if puzzle not solved, rotate the second layer 12 times, etc.
+	// repeat until puzzle is solved or all layers have been rotated 12 times each.
+	// a check is defined as adding up all numbers in a radial column and seeing if all numbers add up to 42.
+
+	// 0 layer (bottom layer, the one with all "radial rows" having numbers)
 	for a := 0; a < 12; a++ {
 		for b := 0; b < 12; b++ {
 			for c := 0; c < 12; c++ {
 				for d := 0; d < 12; d++ {
+					// layer 5 (top layer, the one with the least numbers)
 					for e := 0; e < 12; e++ {
 						if gc.IsSolved() {
 							fmt.Println("Puzzle solved!")
@@ -79,13 +93,7 @@ func (gc *GrecianComputer) Solve() {
 	gc.PrintRotationCounts(rotationCounts)
 }
 
-// PrintRotationCounts prints the number of rotations for each layer.
-func (gc *GrecianComputer) PrintRotationCounts(rotationCounts [5]int) {
-	for i, count := range rotationCounts {
-		fmt.Printf("Layer %d: %d rotations\n", i+1, count)
-	}
-}
-
+// IsSolved checks if the puzzle is completely solved by adding up all numbers in each radial column and checking if they add up to 42.
 func (gc *GrecianComputer) IsSolved() bool {
 	for column := 0; column < 12; column++ {
 		colSum := gc.ColumnSum(column)
@@ -95,6 +103,8 @@ func (gc *GrecianComputer) IsSolved() bool {
 	}
 	return true
 }
+
+// RotateLayer rotates a layer clockwise by one position (as a person would do on the real puzzle).
 func (gc *GrecianComputer) RotateLayer(layerIndex int) {
 	var layer = gc.Puzzle[layerIndex]
 	var newLayer [4][12]int
@@ -105,6 +115,8 @@ func (gc *GrecianComputer) RotateLayer(layerIndex int) {
 	}
 	gc.Puzzle[layerIndex] = newLayer
 }
+
+// ReadValue returns the number located at the coordinates, as defined by a row and a column.
 func (gc *GrecianComputer) ReadValue(row int, column int) int {
 	for layer := 0; layer < len(gc.Puzzle); layer++ {
 		if value := gc.Puzzle[layer][row][column]; value != -1 {
@@ -113,9 +125,13 @@ func (gc *GrecianComputer) ReadValue(row int, column int) int {
 	}
 	return -1
 }
+
+// ColumnSum returns the sum of all numbers in a radial column.
 func (gc *GrecianComputer) ColumnSum(column int) int {
 	return gc.ReadValue(0, column) + gc.ReadValue(1, column) + gc.ReadValue(2, column) + gc.ReadValue(3, column)
 }
+
+// PrintAllLayers prints all layers of the puzzle.
 func (gc *GrecianComputer) PrintAllLayers() {
 	for _, layer := range gc.Puzzle {
 		for _, row := range layer {
@@ -131,7 +147,16 @@ func (gc *GrecianComputer) PrintAllLayers() {
 		fmt.Println()
 	}
 }
-func (gc *GrecianComputer) Print() {
+
+// PrintRotationCounts prints the number of rotations for each layer.
+func (gc *GrecianComputer) PrintRotationCounts(rotationCounts [5]int) {
+	for i, count := range rotationCounts {
+		fmt.Printf("Layer %d: %d rotations\n", i+1, count)
+	}
+}
+
+// PrintPuzzle prints the current state of the puzzle in a human-readable format, complete with radial column sums.
+func (gc *GrecianComputer) PrintPuzzle() {
 	for row := 0; row < 4; row++ {
 		for column := 0; column < 12; column++ {
 			if value := gc.ReadValue(row, column); value != -1 {
@@ -159,8 +184,8 @@ func (gc *GrecianComputer) Print() {
 
 func main() {
 	var grecianComputer = GrecianComputer{
-		Puzzle: grecianComputerPuzzle,
+		Puzzle: grecianComputerObservationData,
 	}
 	grecianComputer.Solve()
-	grecianComputer.Print()
+	grecianComputer.PrintPuzzle()
 }
